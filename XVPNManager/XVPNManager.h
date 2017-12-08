161b5e4d215d17be599bdd14a8fc217256ac14f2
yarshure <support@abigt.net>
2017-12-08 09:46:39 +0800

    Initial Commit

---
+++ XVPNManager/XVPNManager.h
@@ -0,0 +1,19 @@
+//
+//  XVPNManager.h
+//  XVPNManager
+//
+//  Created by yarshure on 2017/12/8.
+//  Copyright © 2017年 yarshure. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XVPNManager.
+FOUNDATION_EXPORT double XVPNManagerVersionNumber;
+
+//! Project version string for XVPNManager.
+FOUNDATION_EXPORT const unsigned char XVPNManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XVPNManager/PublicHeader.h>
+
+
